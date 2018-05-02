09f4ba180f1c9d253cb5a657a7216b7681bc25ba
MarkDino21 <markdinopelonia447@gmail.com>
2018-05-02 14:08:01 +0800

    version 2.1 published and bug for lower series fixed

--- Cash Voucher/My Project/AssemblyInfo.vb
+++ Cash Voucher/My Project/AssemblyInfo.vb
@@ -18,7 +18,7 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("1aa2243e-fd00-48c6-8ec8-f9a73b82b4b5")> 
+<Assembly: Guid("1aa2243e-fd00-48c6-8ec8-f9a73b82b4b5")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("2.1.0.0")>
+<Assembly: AssemblyFileVersion("2.1.0.0")>
